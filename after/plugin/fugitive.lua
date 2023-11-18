62c5ccde70f0b39905ca96917b978c1f5ed415b6
V-Fries <vincentfries@proton.me>
2023-11-18 14:19:12 +0100

    Removed useless comments

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,12 +1,8 @@
 vim.keymap.set("n", "<leader>g", vim.cmd.Git)
 
---local bufnr = vim.api.nvim_get_current_buf()
---local opts = {buffer = bufnr, remap = false}
-
 vim.keymap.set("n", "<leader>p", function()
     vim.cmd.Git('push')
 end)
-
 
 vim.keymap.set("n", "<leader>l", function()
     vim.cmd.Git('pull --rebase')
