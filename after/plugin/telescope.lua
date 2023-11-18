8361956bc76415fb0e33f37870ccdff058e54d5e
V-Fries <vincentfries@proton.me>
2023-11-18 14:26:02 +0100

    Removed useless comment

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,5 +4,5 @@ vim.keymap.set('n', '<leader>O', builtin.git_files, {})
 
 vim.keymap.set('n', '<leader>f', function()
     builtin.grep_string({ search = vim.fn.input("Find > ") })
-end) -- TODO fix this
+end)
 
