49af6a10a20969f8b52b67919c11cbc7cef6ad75
V-Fries <vincentfries@proton.me>
2025-06-11 22:58:36 +0800

    feat: lualine

--- lua/plugins/import.lua
+++ lua/plugins/import.lua
@@ -2,5 +2,6 @@ return {
     { import = "plugins.coding" },
     { import = "plugins.git" },
     { import = "plugins.navigation" },
-    { import = "plugins.quality_of_life" }
+    { import = "plugins.quality_of_life" },
+    { import = "plugins.ui" },
 }
