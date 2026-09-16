4a3b7ef7428186a308b0b008e90ae2d96b126b40
Vincent Fries <github.herd7@aleeas.com>
2026-09-16 08:22:07 +0200

    refactor: renamed lazy.lua to setup_lazy_nvim.lua

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,7 +1,7 @@
 require("config.set")
 
--- Make sure to require config.keymaps before config.lazy because we need to set `mapleader` and
--- `maplocalleader` before loading lazy.nvim
+-- Make sure to require config.keymaps before config.setup_lazy_nvim because we need to set
+-- `mapleader` and `maplocalleader` before loading lazy.nvim
 require("config.keymaps")
 
-require("config.lazy")
+require("config.setup_lazy_nvim")
